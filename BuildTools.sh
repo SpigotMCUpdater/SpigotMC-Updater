929320395908e1b99c050826de71a573b4f2fa69
Nato Boram <NatoBoram@users.noreply.github.com>
2016-05-06 11:30:04 -0400

    Moved License

--- BuildTools.sh
+++ BuildTools.sh
@@ -1,3 +1,4 @@
+curl -o License.txt --progress-bar https://raw.githubusercontent.com/Legoman99573/SpigotMC-Updater/master/License.md
 read -t60 -n1 -r -p "SpigotMC Updater : Updates everything Spigot-related. Copyright (C) 2016 NatoBoram
 
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -5,7 +6,6 @@ This program is free software: you can redistribute it and/or modify it under th
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/."
-curl -o License.txt --progress-bar https://raw.githubusercontent.com/Legoman99573/SpigotMC-Updater/master/License.md
 curl -o BuildTools.jar --progress-bar https://hub.spigotmc.org/jenkins/job/BuildTools/lastBuild/artifact/target/BuildTools.jar
 curl -o BungeeCord.jar --progress-bar http://ci.md-5.net/job/BungeeCord/lastBuild/artifact/bootstrap/target/BungeeCord.jar
 java -jar BuildTools.jar --rev latest
